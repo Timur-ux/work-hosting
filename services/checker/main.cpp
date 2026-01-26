947ae23def94b4d0a7a3751ea7043f7d6c298227
Tias <timur.buchkin@mail.ru>
2026-01-26 15:46:32 +0300

    work on checker service

---
+++ services/checker/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <zmq.h>
+
+int main(int argc, const char * argw[]) {
+	
+	return 0;
+}
--- src/Work.cpp
+++ src/Work.cpp
@@ -1,6 +1,10 @@
 #include "work.hpp"
+#include <exception>
 #include <format>
+#include <limits>
 #include <stdexcept>
+#include <string>
+#include <string_view>
 
 Work::Work(Type type, unsigned short number, const std::string &gvName)
 	: type_(type), number_(number), gvName_(gvName) {
@@ -29,4 +33,31 @@ std::string Work::to_string_() const {
 } 
 const std::string & Work::to_string() const {
 	return stringView_;
+} 
+
+Work Work::from_string(std::string_view s) {
+	Type type;
+	if(s.starts_with("LR")) 
+		type = Type::LR;
+	else if (s.starts_with("KP") == 0)
+	 type = Type::KP;
+	else
+		throw std::invalid_argument(std::format("Work type is invalid! Expected LR or KP but given: {}", s.substr(0, 2)));
+	size_t defis = s.find("-");
+	if(defis == std::string::npos) 
+		throw std::invalid_argument("Expected '-' between work number and username but '-' not found!");
+	if(defis <= 2) 
+		throw std::invalid_argument("Expected work number after work type but it is elapsed!");
+	long workNumber;
+	try {
+		workNumber = std::stol(std::string(s.substr(2, defis - 2)), nullptr, 10);
+	} catch (...) {
+		throw std::invalid_argument("Error while converting work number from string to integer");
+	}
+	if(!(workNumber > 0 && workNumber < std::numeric_limits<unsigned short>::max())) 
+		throw std::invalid_argument("Work number must be in unsigned short range");
+	
+	std::string_view gvName = s.substr(defis+1);
+
+	return Work{type, static_cast<unsigned short>(workNumber), std::string(gvName)};
 } 
