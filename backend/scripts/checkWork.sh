f9e3daec75d6df6ef130b30d2cfe3fcab10cceaa
Tias <timur.buchkin@mail.ru>
2026-04-05 15:28:28 +0300

    add deleting existing object and executable files

--- backend/scripts/checkWork.sh
+++ backend/scripts/checkWork.sh
@@ -240,6 +240,7 @@ checkMakeBuild() {
     return 1
   fi
 
+	find . -delete -name "*.o" -or -name "*.out" -or -name "*.exe"
   message=$(sed 's/gcc/gcc $(SOME_BUILD_FLAGS)/' -i ./Makefile 2>/dev/stdout)
   if [ "$?" != "0" ]; then
     echo "$message"
